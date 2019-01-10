c4b157b9a9aa247532623f61742fea0bd3c58d9a
cclauss <cclauss@me.com>
2019-01-10 22:01:49 +0100

    Simplify first/last logic

--- leaklooker.py
+++ leaklooker.py
@@ -23,7 +23,7 @@ LeakLooker - Find open databases
 https://medium.com/@woj_ciech https://github.com/woj-ciech/
 Example: python leaklooker.py --mongodb --couchdb --kibana --elastic --first 21 --last 37"""
 
-print (description)
+print(description)
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter #added to show default value
@@ -47,20 +47,14 @@ kibana = args.kibana
 first = args.first
 last = args.last
 
-if first and last is None:
-    print ("Correct pages")
-    sys.exit()
-elif last and first is None:
-    print ('Correct pages')
-    sys.exit()
 elif first is None and last is None:
-    print ("Choose pages to search")
+    print("Choose pages to search")
     sys.exit()
-elif first > last:
-    print ('Correct pages')
+elif first is None or last is None or first > last:
+    print('Correct pages')
     sys.exit()
 else:
-    last = last+1
+    last += 1
 
 SHODAN_API_KEY = ''
 
@@ -75,14 +69,14 @@ def shodan_query(query, page):
         api = shodan.Shodan(SHODAN_API_KEY)
         result = api.search(query, page=page)
     except shodan.APIError as e:
-        print (Fore.RED + e.value + Fore.RESET)
+        print(Fore.RED + e.value + Fore.RESET)
         return False
 
     if len(result['matches']) > 0:
-        print ('Found ' + str(result['total']) + " results")
+        print('Found ' + str(result['total']) + " results")
 
     else:
-        print ("Nothing was found")
+        print("Nothing was found")
         return False
 
     return result
@@ -92,16 +86,16 @@ def check_elastic(results):
         for service in results['matches']:
             try:
                 if service['elastic']['cluster']['indices']['store']['size_in_bytes'] > 217000000:
-                    print ("IP: http://" + Fore.LIGHTGREEN_EX  + service['ip_str'] + ':' + str(service['port']) + '/_cat/indices?v' + Fore.RESET)
-                    print ("Size: "+ Fore.LIGHTGREEN_EX  + size(service['elastic']['cluster']['indices']['store']['size_in_bytes']) + Fore.RESET)
+                    print("IP: http://" + Fore.LIGHTGREEN_EX  + service['ip_str'] + ':' + str(service['port']) + '/_cat/indices?v' + Fore.RESET)
+                    print("Size: "+ Fore.LIGHTGREEN_EX  + size(service['elastic']['cluster']['indices']['store']['size_in_bytes']) + Fore.RESET)
                     try:
                         print('Country: ' + Fore.LIGHTBLUE_EX + service['location']['country_name'] + Fore.RESET)
                     except:
-                        print ('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
-                    print ("Indices: ")
+                        print('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
+                    print("Indices: ")
                     for indice,info in service['elastic']['indices'].items():
-                        print (Fore.GREEN + indice + Fore.RESET)
-                    print ("-----------------------------")
+                        print(Fore.GREEN + indice + Fore.RESET)
+                    print("-----------------------------")
             except KeyError:
                 pass
 
@@ -111,20 +105,20 @@ def check_mongodb(results):
 
             try:
                 if service['mongodb']['listDatabases']['totalSize'] > 217000000:
-                    print ("IP: " + Fore.LIGHTBLUE_EX + service['ip_str'] + Fore.RESET)
-                    print ("Size: " + Fore.LIGHTBLUE_EX + size(service['mongodb']['listDatabases']['totalSize']) + Fore.RESET)
+                    print("IP: " + Fore.LIGHTBLUE_EX + service['ip_str'] + Fore.RESET)
+                    print("Size: " + Fore.LIGHTBLUE_EX + size(service['mongodb']['listDatabases']['totalSize']) + Fore.RESET)
                     try:
                         print('Country: ' + Fore.LIGHTBLUE_EX + service['location']['country_name'] + Fore.RESET)
                     except:
-                        print ('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
+                        print('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
                     for database in service['mongodb']['listDatabases']['databases']:
                         if database['empty'] != 'true':
-                            print ("Database name: " + Fore.BLUE + database['name'] + Fore.RESET)
+                            print("Database name: " + Fore.BLUE + database['name'] + Fore.RESET)
                             print("Size: " + Fore.BLUE + size(database['sizeOnDisk']) + Fore.RESET)
-                            print ('Collections: ')
+                            print('Collections: ')
                             for collection in database['collections']:
-                                print (Fore.LIGHTBLUE_EX + collection + Fore.RESET)
-                    print ("-----------------------------")
+                                print(Fore.LIGHTBLUE_EX + collection + Fore.RESET)
+                    print("-----------------------------")
             except KeyError:
                 pass
 
@@ -139,16 +133,16 @@ def check_couchdb(results):
                         if line.startswith("{"):
                             json_data = json.loads(line)
                             if len(json_data['dbs']) < 20 and 'compromised' not in service['tags']:
-                                print ("IP: http://" + Fore.YELLOW + service['ip_str'] + ':' + str(service['port']) + '/_utils' + Fore.RESET)
+                                print("IP: http://" + Fore.YELLOW + service['ip_str'] + ':' + str(service['port']) + '/_utils' + Fore.RESET)
                                 try:
                                     print('Country: ' + Fore.LIGHTBLUE_EX + service['location'][
                                         'country_name'] + Fore.RESET)
                                 except:
                                     print('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
                                 for db in json_data['dbs']:
-                                    print (Fore.LIGHTYELLOW_EX + db + Fore.RESET)
+                                    print(Fore.LIGHTYELLOW_EX + db + Fore.RESET)
 
-                            print ("-----------------------------")
+                            print("-----------------------------")
             except KeyError:
                 pass
 
@@ -162,33 +156,33 @@ def check_kibana(results):
                     try:
                         print('Country: ' + Fore.LIGHTBLUE_EX + service['location']['country_name'] + Fore.RESET)
                     except:
-                        print ('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
-                        print ('---')
-                    print ('---')
+                        print('Country: ' + Fore.RED + 'Unknown' + Fore.RESET)
+                        print('---')
+                    print('---')
         except:
             pass
 
 
 if elastic:
     for page in range (first,last):
-        print (Fore.RED + '----------------------------------Elastic - Page ' + str(page) + '--------------------------------' + Fore.RESET)
+        print(Fore.RED + '----------------------------------Elastic - Page ' + str(page) + '--------------------------------' + Fore.RESET)
         elastic_results = shodan_query(query_elastic, page)
         check_elastic(elastic_results)
 
 if couchdb:
     for page in range (first,last):
-        print (Fore.RED + '----------------------------------CouchDB - Page ' + str(page) + '--------------------------------' + Fore.RESET)
+        print(Fore.RED + '----------------------------------CouchDB - Page ' + str(page) + '--------------------------------' + Fore.RESET)
         couchdb_results = shodan_query(query_couchdb, page)
         check_couchdb(couchdb_results)
 
 if mongodb:
     for page in range (first,last):
-        print (Fore.RED + '----------------------------------MongoDB - Page ' + str(page) + '--------------------------------' + Fore.RESET)
+        print(Fore.RED + '----------------------------------MongoDB - Page ' + str(page) + '--------------------------------' + Fore.RESET)
         mongodb_results = shodan_query(query_mongodb, page)
         check_mongodb(mongodb_results)
 
 if kibana:
     for page in range (first,last):
-        print (Fore.RED + '----------------------------------Kibana - Page ' + str(page) + '--------------------------------' + Fore.RESET)
+        print(Fore.RED + '----------------------------------Kibana - Page ' + str(page) + '--------------------------------' + Fore.RESET)
         kibana_results = shodan_query(query_kibana, page)
         check_kibana(kibana_results)
